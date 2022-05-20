743271279eb96fb9d4f17f86e4d3aae063a19675
shu10111 <pkathy111@gmail.com>
2022-05-21 02:08:48 +0800

    add test.txt

--- readfile/readfile1.js
+++ readfile/readfile1.js
@@ -1,16 +1,36 @@
 const fs = require('fs');
+// fs.readFile('test11111.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     // 錯誤了
+//     console.log('喔喔喔，發生錯誤了');
+//     console.error(err);
+//   } else {
+//     // 因為沒有 err，所以是正確的
+//     console.log(data);
+//   }
+// });
 
-fs.readFile('test11111.txt', 'utf-8', (err, data) => {
-  if (err) {
-    // 錯誤了
-    console.log('喔喔喔，發生錯誤了');
-    console.error(err);
-  } else {
-    // 因為沒有 err，所以是正確的
-    console.log(data);
-  }
+//new Promise((resovle, reject) => {
+
+let p = new Promise((resolve, reject) => {
+  fs.readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      // 錯誤了
+      //console.log('喔喔喔，發生錯誤了');
+      //console.error(err);
+      reject(err);
+    } else {
+      // 因為沒有 err，所以是正確的
+      //console.log(data);
+      resolve(data);
+    }
+  });
 });
-async function main(){
-    let 
+p.then((result)=>{
+  console.log(result);
 
-}
+}).catch((err) => {
+  console.error(err);
+});
+
+
