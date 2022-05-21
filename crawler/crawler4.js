9e6efb85717d1b665d1691e6178c74914df6b9ee
shu10111 <pkathy111@gmail.com>
2022-05-21 14:41:06 +0800

    add example

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -4,15 +4,19 @@
 // npm i mysql2
 // 引用進來
 const mysql = require('mysql2/promise');
+//const dotenv = require('dotenv')
+require('dotenv').config();
 
+//console.log(process.env.DB_HOST)
 (async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: '12345',
-    database: 'node_js_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_POST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
+
 
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
   console.log(data);
