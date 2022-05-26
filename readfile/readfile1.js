3c8a4c59da2d70578c23c8512c2d2209ef723a27
shu10111 <pkathy111@gmail.com>
2022-05-27 00:40:50 +0800

    重新練習

--- readfile/readfile1.js
+++ readfile/readfile1.js
@@ -1,36 +1,10 @@
 const fs = require('fs');
-// fs.readFile('test11111.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     // 錯誤了
-//     console.log('喔喔喔，發生錯誤了');
-//     console.error(err);
-//   } else {
-//     // 因為沒有 err，所以是正確的
-//     console.log(data);
-//   }
-// });
 
-//new Promise((resovle, reject) => {
+fs.readFile('test.txt','utf-8',(err, data) => {
+  if (err){
+    console.error(err)
 
-let p = new Promise((resolve, reject) => {
-  fs.readFile("test.txt", "utf-8", (err, data) => {
-    if (err) {
-      // 錯誤了
-      //console.log('喔喔喔，發生錯誤了');
-      //console.error(err);
-      reject(err);
-    } else {
-      // 因為沒有 err，所以是正確的
-      //console.log(data);
-      resolve(data);
-    }
-  });
-});
-p.then((result)=>{
-  console.log(result);
-
-}).catch((err) => {
-  console.error(err);
-});
-
-
+  }else{
+    console.log(data)
+  }
+})
