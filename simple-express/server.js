5846aa82b854d6d6ac9e7d3ee2727741dc318fe0
shu10111 <pkathy111@gmail.com>
2022-05-27 20:04:46 +0800

    add .gitignore

---
+++ simple-express/server.js
@@ -0,0 +1,22 @@
+//npm i express
+//引用 導入 express 這個模組
+const express = require('express');
+//利用express 來建立一個 express application
+const app = express();
+//http request
+//method: get, post, put, delete...
+app.get('/', (request, response, next) => {
+  response.send('首頁');
+});
+
+app.get('/about', (request, response, next) => {
+  response.send('About Me');
+});
+
+app.get('/product', (request, response, next) => {
+  response.send('商品');
+});
+
+app.listen(3001, () => {
+  console.log('server start at 3001');
+});
